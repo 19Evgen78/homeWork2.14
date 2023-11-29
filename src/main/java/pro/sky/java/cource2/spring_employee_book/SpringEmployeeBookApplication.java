7b14d409651584c215cc5f56fd050b4fccbade0b
19kuz <19kuzminykh78@mail.ru>
2023-11-29 12:18:30 +0800

    ДЗ 2.14

---
+++ src/main/java/pro/sky/java/cource2/spring_employee_book/SpringEmployeeBookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.cource2.spring_employee_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEmployeeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmployeeBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/cource2/spring_employee_book/SpringEmployeeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.cource2.spring_employee_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
